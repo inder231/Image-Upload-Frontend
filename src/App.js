c101874c28c7f1f63edca0b4a386f5d33e7669c0
Inder Pal <101566674+Inder-Pal-github@users.noreply.github.com>
2023-06-23 13:01:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Input, Button } from "@chakra-ui/react";
+import { FormControl, FormLabel, FormHelperText } from "@chakra-ui/react";
+import { Alert, AlertIcon, Avatar, Wrap } from "@chakra-ui/react";
+
+export default function App() {
+  const [file, setFile] = useState();
+  const [isFieldEmpty, setIsFieldEmpty] = useState(false);
+  const [isAvatar, setIsAvatar] = useState(false);
+  async function uploadFile(event) {
+    event.preventDefault();
+    // console.log(file);
+    if (!file) {
+      return setIsFieldEmpty(true);
+    } else {
+      const payload = new FormData();
+      payload.append("profile", file);
+
+      try {
+        const { data } = await axios.post(
+          "https://cerulean-ant-slip.cyclic.app/upload/profile",
+          payload
+        );
+        setIsAvatar(data.url);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+  function handleChange(e) {
+    // console.log(e.target.files);
+    if (e.target.files[0]) {
+      setFile(e.target.files[0]);
+      setIsFieldEmpty(false);
+    }
+  }
+  async function checkServer() {
+    const { data } = await axios.get("https://cerulean-ant-slip.cyclic.app/");
+    console.log(data);
+  }
+  useEffect(() => {
+    checkServer();
+  }, []);
+  return (
+    <div className="App">
+      <Wrap
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        margin="1rem"
+      >
+        {isAvatar && <Avatar size="2xl" src={isAvatar} />}
+      </Wrap>
+      <form onSubmit={uploadFile}>
+        <FormControl>
+          <FormLabel>Profile Upload</FormLabel>
+          <Input size="md" onChange={handleChange} type="file" name="profile" />
+          <Button
+            type="submit"
+            colorScheme="teal"
+            variant="solid"
+            margin="10px"
+          >
+            Upload
+          </Button>
+          <FormHelperText>Image will be uploaded to cloudinary.</FormHelperText>
+        </FormControl>
+      </form>
+      {isFieldEmpty && (
+        <Alert status="error" mb={4}>
+          <AlertIcon />
+          Please fill in the required field.
+        </Alert>
+      )}
+    </div>
+  );
+}
